b04876f2c0f80c1d132ee0f9a51f939f28ef3be5
권태용 <dkty0311@naver.com>
2019-12-15 15:36:45 +0900

    5

---
+++ 포인터/3번/3번.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void array_fill(int* A, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		*(A + i) = rand();
+	}
+}
+
+int main()
+{
+	int a[5] = { 0 };
+
+	array_fill(a, 5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
---
+++ 포인터/4번/4번.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void arrat_print(int* A, int size)
+{
+	int i;
+	printf("A[]={ ");
+	for (i = 0; i < size; i++)
+	{
+
+	}
+	printf("A[]= }\n");
+}
+
+int main()
+{
+	
+}
