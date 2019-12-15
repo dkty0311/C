198144d38e446fb49a8aca82a737a344b0496f8b
1961003@pcu.ac.kr <1961003@pcu.ac.kr>
2019-12-15 17:23:01 +0900

    sadsa

--- 포인터/4번/4번.c
+++ 포인터/4번/4번.c
@@ -6,12 +6,14 @@ void arrat_print(int* A, int size)
 	printf("A[]={ ");
 	for (i = 0; i < size; i++)
 	{
-
+		printf("%d", *(A + i));
 	}
 	printf("A[]= }\n");
 }
 
 int main()
 {
-	
+	int a[8] = { 1,2,3,4,5,6 };
+
+	arrat_print(a, sizeof(a[8]));
 }
